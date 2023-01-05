28c678f993768b9e5a26a69979c6a72e6a1c14d2
gfgafn <87858323+gfgafn@users.noreply.github.com>
2023-01-05 18:42:09 +0800

    feat: Rays, a Simple Camera, and Background

--- in_one_weekend/src/main.rs
+++ in_one_weekend/src/main.rs
@@ -1,26 +1,43 @@
 mod color;
 mod point;
+mod ray;
 mod vec3;
 
 use std::{fs::File, io::Write};
 
-use color::ColorRGB;
+use crate::{color::ColorRGB, point::Point3, ray::Ray, vec3::Vec3};
 
 fn main() -> std::io::Result<()> {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    // Image
+    const ASPECT_RATIO: f32 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as u32;
 
+    // Camera
+    const VIEWPORT_HEIGHT: f32 = 2.0;
+    const VIEWPORT_WIDTH: f32 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f32 = 1.0;
+
+    const ORIGIN: Point3 = Vec3(0.0, 0.0, 0.0);
+    const HORIZONTAL: Vec3 = Vec3(VIEWPORT_WIDTH, 0.0, 0.0);
+    const VERTICAL: Vec3 = Vec3(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner: Point3 =
+        ORIGIN - HORIZONTAL / 2.0 - VERTICAL / 2.0 - Vec3(0.0, 0.0, FOCAL_LENGTH);
+
+    // Render
     const OUTPUT_IMAGE_PATH: &'static str = "./target/image.ppm";
     let mut image = File::create(OUTPUT_IMAGE_PATH)?;
     image.write(format!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n").as_bytes())?;
     for j in (0..IMAGE_HEIGHT).rev() {
         print!("\rScanlines remaining: {j}");
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = ColorRGB::new(
-                (255.0 * i as f32 / (IMAGE_WIDTH - 1) as f32) as u8,
-                (255.0 * j as f32 / (IMAGE_HEIGHT - 1) as f32) as u8,
-                (0.25 * 255.0) as u8,
+            let u = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let v = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let ray = Ray::new(
+                ORIGIN,
+                lower_left_corner + u * HORIZONTAL + v * VERTICAL - ORIGIN,
             );
+            let pixel_color: ColorRGB = ray_color(ray);
             write_color(&mut image, pixel_color)?;
         }
     }
@@ -32,4 +49,10 @@ fn main() -> std::io::Result<()> {
 fn write_color(file: &mut std::fs::File, color: ColorRGB) -> std::io::Result<()> {
     file.write(format!("{} {} {}\n", color.r(), color.g(), color.b()).as_bytes())?;
     Ok(())
+}
+
+fn ray_color(r: Ray) -> ColorRGB {
+    let unit_direction: Vec3 = r.direction().unit_vector();
+    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * ColorRGB::from_binary(1.0, 1.0, 1.0) + t * ColorRGB::from_binary(0.5, 0.7, 1.0)
 }
