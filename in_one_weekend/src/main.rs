44182e6c573d4c3c4ec3cd63420614fa8d7fbd96
gfgafn <87858323+gfgafn@users.noreply.github.com>
2023-01-14 23:21:14 +0800

    feat: Total Internal Reflection

--- in_one_weekend/src/main.rs
+++ in_one_weekend/src/main.rs
@@ -37,9 +37,9 @@ fn main() -> std::io::Result<()> {
     let mut world: HittableList<Arc<dyn Hittable>> = HittableList::new();
 
     let material_ground = Arc::new(Lambertian::new(Attenuation::new(Vec3::new(0.8, 0.8, 0.0))));
-    let material_center = Arc::new(Dielectric::new(1.5));
+    let material_center = Arc::new(Lambertian::new(Attenuation::new(Vec3::new(0.1, 0.2, 0.5))));
     let material_left = Arc::new(Dielectric::new(1.5));
-    let material_right = Arc::new(Metal::new(Attenuation::new(Vec3::new(0.8, 0.6, 0.2)), 1.0));
+    let material_right = Arc::new(Metal::new(Attenuation::new(Vec3::new(0.8, 0.6, 0.2)), 0.0));
 
     world.add(Arc::new(Sphere::new(
         Point3::new(0., -100.5, -1.),
--- in_one_weekend/src/material/dielectric.rs
+++ in_one_weekend/src/material/dielectric.rs
@@ -1,6 +1,6 @@
 use crate::{hittable::HitRecord, ray::Ray, vec3::Vec3};
 
-use super::{refract, Attenuation, Scatter, ScatterRecord};
+use super::{reflect, refract, Attenuation, Scatter, ScatterRecord};
 
 pub struct Dielectric {
     // Index of Refraction
@@ -21,14 +21,21 @@ impl Scatter for Dielectric {
             false => self.ior,
         };
 
-        let ray_refracted_direction: Vec3 = refract(
-            ray_in.direction().unit_vector(),
-            hit_record.normal(),
-            refract_ration,
-        );
+        let ray_in_dir_unit: Vec3 = ray_in.direction().unit_vector();
+        let cos_theta: f32 = f32::min((-ray_in_dir_unit).dot(hit_record.normal()), 1.0);
+        let sin_theta: f32 = (1.0 - cos_theta.powi(2)).abs().sqrt();
+
+        let direction: Vec3 = match 1.0 < refract_ration * sin_theta {
+            true => reflect(ray_in_dir_unit, hit_record.normal()),
+            false => refract(
+                ray_in.direction().unit_vector(),
+                hit_record.normal(),
+                refract_ration,
+            ),
+        };
 
         Some(ScatterRecord::new(
-            Ray::new(hit_record.position(), ray_refracted_direction),
+            Ray::new(hit_record.position(), direction),
             Attenuation::new(Vec3::new(1.0, 1.0, 1.0)),
         ))
     }
