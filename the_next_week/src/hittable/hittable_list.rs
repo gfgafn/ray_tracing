501182eeaea5417555535647c429dc9839f8f77a
gfgafn <87858323+gfgafn@users.noreply.github.com>
2023-02-20 23:07:26 +0800

    refactor: more functional

--- the_next_week/src/hittable/hittable_list.rs
+++ the_next_week/src/hittable/hittable_list.rs
@@ -33,17 +33,19 @@ impl<H: AsRef<dyn Hittable>> Default for HittableList<H> {
 
 impl<H: AsRef<dyn Hittable> + Send + Sync> Hittable for HittableList<H> {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
-        let mut hit: Option<HitRecord> = None;
-        let mut closest_so_far: f32 = t_max;
-
-        self.objects.iter().for_each(|obj: &H| {
-            if let Some(hit_record) = obj.as_ref().hit(ray, t_min, closest_so_far) {
-                debug_assert!(t_min <= hit_record.t && hit_record.t <= closest_so_far);
-                closest_so_far = hit_record.t;
-                hit = Some(hit_record);
-            }
-        });
-
-        hit
+        self.objects
+            .iter()
+            .fold(
+                (None, t_max),
+                |(mut current_hit_record, mut closest_so_far), object: &H| {
+                    if let Some(hit_record) = object.as_ref().hit(ray, t_min, closest_so_far) {
+                        debug_assert!(t_min <= hit_record.t && hit_record.t <= closest_so_far);
+                        closest_so_far = hit_record.t;
+                        current_hit_record = Some(hit_record);
+                    }
+                    (current_hit_record, closest_so_far)
+                },
+            )
+            .0
     }
 }
